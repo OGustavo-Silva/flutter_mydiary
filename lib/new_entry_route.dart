808aa94314a81abb47028ccf97b03f1e4b651c0a
Gustavo Silva <gustavos_barros@hotmail.com>
2024-01-28 13:22:03 -0300

    added style to forms on new entry route

--- lib/new_entry_route.dart
+++ lib/new_entry_route.dart
@@ -14,8 +14,8 @@ class _NewEntryRouteState extends State<NewEntryRoute> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("teste"),
-      ),
+          title: const Text("New Entry"),
+          backgroundColor: const Color.fromARGB(199, 255, 255, 255)),
       body: Center(
         child: Column(
           children: [
@@ -23,23 +23,53 @@ class _NewEntryRouteState extends State<NewEntryRoute> {
               child: Column(
                 children: <Widget>[
                   TextFormField(
-
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                        hintText: "Title",
+                        hintStyle: TextStyle(
+                            color: Color.fromARGB(99, 164, 164, 164))),
                   ),
                   TextFormField(
-
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                        hintText: "Content",
+                        hintStyle: TextStyle(
+                            color: Color.fromARGB(99, 164, 164, 164))),
+                  ),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(100, 150, 10, 0),
+                          foregroundColor:
+                              const Color.fromARGB(199, 255, 255, 255),
+                        ),
+                        child: const Text("Discard"),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(Icons.check),
+                        label: const Text("Save"),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(100, 0, 150, 10),
+                          foregroundColor:
+                              const Color.fromARGB(199, 255, 255, 255),
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text("Discard"),
-            ),
           ],
         ),
       ),
+      backgroundColor: const Color.fromARGB(0, 56, 56, 56),
     );
   }
 }
