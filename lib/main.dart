cbe745d928210d936935b4d1f7bfd601660e4d7b
Gustavo Silva <gustavos_barros@hotmail.com>
2024-02-03 13:18:15 -0300

    WorkingOnAutoConfigOfDirectoriesPerDate

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_diary/new_entry_route.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import  'package:path/path.dart' as path;
 
 void main() {
   createFolder();
@@ -14,29 +15,53 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-  
+
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
   }
 }
 
-//Vars for setting folders configs
+//Vars for files/directories config
 const title = "MyDiary";
 String fullYear = DateTime.now().toString();
 String year = fullYear.substring(0, 4);
-String month = fullYear.substring(5,7);
+String month = fullYear.substring(5, 7);
 String myDiaryPath = "$title/$year/$month";
 
-void createFolder() async{
+void createFolder() async {
   final Directory directory = await getApplicationDocumentsDirectory();
   File('${directory.path}/$myDiaryPath/myJournal.txt').create(recursive: true);
+}
+
+void checkExistingNotes() async {
+  final Directory directory = await getApplicationDocumentsDirectory();//Return Documents folder equivalent
+  final stat = FileStat.statSync('${directory.path}/${title}');
+  print("myDiary folder stat ${stat.changed}");
+
+  final diaryDir = "${directory.path}/${title}";
+  final List<FileSystemEntity> homeDirItems = await Directory(diaryDir).list().toList();
+  //homeDirItems.forEach((element) {print(element);}); //print every file/dir in myDiary folder
+
+  final Iterable<Directory> homeDirDirectories = homeDirItems.whereType<Directory>();
+  List<String> dirsList = [];
+  homeDirDirectories.forEach((element) {
+    var dirName = element.toString();
+    dirsList.add(path.basename(dirName.substring(0, dirName.length-1)));
+    });
+  dirsList.sort();
+  dirsList.forEach((element) {print(element.toString());});
+
+  //run over years directories
+  for(int i = dirsList.length-1; i > 0; i--){
+    var year_directory = Directory("${directory.path}/${title}/${dirsList[i]}") ;
+
+    final List<FileSystemEntity> yearDirItems = await Directory(year_directory.path).list().toList();
+    final Iterable<Directory> yearDirDirectories = yearDirItems.whereType<Directory>();
+
+    
   }
 
-void checkExistingNotes() async{
-  final Directory directory = await getApplicationDocumentsDirectory();
-  final stat = FileStat.statSync('${directory.path}/${title}');
-  print("oia a pedra ${stat.changed}");
 }
 
 class _MyAppState extends State<MyApp> {
@@ -44,28 +69,25 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text(title),
-            centerTitle: true,
-            backgroundColor: const Color.fromARGB(199, 255, 255, 255)),
-        body: Center(
-          child: Text(
-            text,
-            style: const TextStyle(color: Colors.white),
-          ),
+      appBar: AppBar(
+          title: const Text(title),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(199, 255, 255, 255)),
+      body: Center(
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.white),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context)=> const NewEntryRoute())
-            );
-          },
-          backgroundColor: const Color.fromARGB(199, 255, 255, 255),
-          child: const Icon(Icons.add),
-        ),
-        backgroundColor: const Color.fromARGB(0, 56, 56, 56),
-      );
-    
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const NewEntryRoute()));
+        },
+        backgroundColor: const Color.fromARGB(199, 255, 255, 255),
+        child: const Icon(Icons.add),
+      ),
+      backgroundColor: const Color.fromARGB(0, 56, 56, 56),
+    );
   }
 }
