d3149aa4b389274f578c18a46e2d2a1cb15e9d56
Gustavo Silva <gustavos_barros@hotmail.com>
2024-02-04 13:01:59 -0300

    AppReadingTxtFileOKMethodsMovedToAnotherDartFile

---
+++ lib/folders_build.dart
@@ -0,0 +1,79 @@
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+class FoldersBuild {
+  final title = "MyDiary";
+  String fullYear = DateTime.now().toString();
+  String year = "";
+  String month = "";
+  String myDiaryPath = "";
+
+  FoldersBuild() {
+    year = fullYear.substring(0, 4);
+    month = fullYear.substring(5, 7);
+    myDiaryPath = "$title/$year/$month";
+  }
+//Vars for files/directories config
+
+  void createFolder() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    File('${directory.path}/$myDiaryPath/myJournal.txt')
+        .create(recursive: true);
+  }
+
+  void checkExistingNotes() async {
+    final Directory directory =
+        await getApplicationDocumentsDirectory(); //Return Documents folder equivalent
+    final stat = FileStat.statSync('${directory.path}/${title}');
+    print("myDiary folder stat ${stat.changed}");
+
+    final diaryDir = "${directory.path}/${title}";
+    final List<FileSystemEntity> homeDirItems =
+        await Directory(diaryDir).list().toList();
+    //homeDirItems.forEach((element) {print(element);}); //print every file/dir in myDiary folder
+
+    final Iterable<Directory> homeDirDirectories =
+        homeDirItems.whereType<Directory>();
+    List<String> homeDirDirsList = [];
+    homeDirDirectories.forEach((element) {
+      var dirName = element.toString();
+      homeDirDirsList
+          .add(path.basename(dirName.substring(0, dirName.length - 1)));
+    });
+    homeDirDirsList.sort();
+    homeDirDirsList.forEach((element) {
+      print(element.toString());
+    });
+
+    //run over years directories
+    for (int i = homeDirDirsList.length - 1; i >= 0; i--) {
+      var yearDirectory =
+          Directory("${directory.path}/${title}/${homeDirDirsList[i]}");
+
+      final List<FileSystemEntity> yearDirItems =
+          await Directory(yearDirectory.path).list().toList();
+      final Iterable<Directory> yearDirDirectories =
+          yearDirItems.whereType<Directory>();
+
+      List<String> yearDirDirsList = [];
+      yearDirDirectories.forEach((element) {
+        var dirName = element.toString();
+        yearDirDirsList
+            .add(path.basename(dirName.substring(0, dirName.length - 1)));
+      });
+      yearDirDirsList.sort();
+      yearDirDirsList.forEach((element) {
+        print(element);
+      });
+
+      //run over the months directories inside i=year directory and open myJournal.txt
+      for (int j = yearDirDirsList.length - 1; j >= 0; j--) {
+        final File file = File(
+            "${directory.path}/${title}/${homeDirDirsList[i]}/${yearDirDirsList[j]}/myJournal.txt");
+        String notes = await file.readAsString();
+        print(notes);
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:my_diary/folders_build.dart';
 import 'package:my_diary/new_entry_route.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import  'package:path/path.dart' as path;
 
 void main() {
-  createFolder();
+  FoldersBuild foldersBuild = FoldersBuild();
+  foldersBuild.createFolder();
   runApp(const MaterialApp(
     title: "MyDiary",
     home: MyApp(),
   ));
-  checkExistingNotes();
+  foldersBuild.checkExistingNotes();
 }
 
 class MyApp extends StatefulWidget {
@@ -22,55 +24,13 @@ class MyApp extends StatefulWidget {
   }
 }
 
-//Vars for files/directories config
-const title = "MyDiary";
-String fullYear = DateTime.now().toString();
-String year = fullYear.substring(0, 4);
-String month = fullYear.substring(5, 7);
-String myDiaryPath = "$title/$year/$month";
-
-void createFolder() async {
-  final Directory directory = await getApplicationDocumentsDirectory();
-  File('${directory.path}/$myDiaryPath/myJournal.txt').create(recursive: true);
-}
-
-void checkExistingNotes() async {
-  final Directory directory = await getApplicationDocumentsDirectory();//Return Documents folder equivalent
-  final stat = FileStat.statSync('${directory.path}/${title}');
-  print("myDiary folder stat ${stat.changed}");
-
-  final diaryDir = "${directory.path}/${title}";
-  final List<FileSystemEntity> homeDirItems = await Directory(diaryDir).list().toList();
-  //homeDirItems.forEach((element) {print(element);}); //print every file/dir in myDiary folder
-
-  final Iterable<Directory> homeDirDirectories = homeDirItems.whereType<Directory>();
-  List<String> dirsList = [];
-  homeDirDirectories.forEach((element) {
-    var dirName = element.toString();
-    dirsList.add(path.basename(dirName.substring(0, dirName.length-1)));
-    });
-  dirsList.sort();
-  dirsList.forEach((element) {print(element.toString());});
-
-  //run over years directories
-  for(int i = dirsList.length-1; i > 0; i--){
-    var year_directory = Directory("${directory.path}/${title}/${dirsList[i]}") ;
-
-    final List<FileSystemEntity> yearDirItems = await Directory(year_directory.path).list().toList();
-    final Iterable<Directory> yearDirDirectories = yearDirItems.whereType<Directory>();
-
-    
-  }
-
-}
-
 class _MyAppState extends State<MyApp> {
   String text = "hello world";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text(title),
+          title: const Text("MyDiary"),
           centerTitle: true,
           backgroundColor: const Color.fromARGB(199, 255, 255, 255)),
       body: Center(
