b307674b66bcdacd62ba1a5379fa26693e003d22
Gustavo Silva <gustavos_barros@hotmail.com>
2024-03-06 16:12:31 -0300

    AddedStylingToNoteWidget

--- lib/note_widget.dart
+++ lib/note_widget.dart
@@ -7,25 +7,39 @@ class NoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          note.title,
-          style: const TextStyle(color: Colors.white),
-        ),
-        Text(
-          note.content,
-          style: const TextStyle(color: Colors.white),
-        ),
-        Text(
-          note.createdDate.toString(),
-          style: const TextStyle(color: Colors.white),
-        ),
-        Text(
-          note.updatedDate.toString(),
-          style: const TextStyle(color: Colors.white),
-        ),
-      ],
-    );
+    return Container(
+        padding: const EdgeInsets.all(5),
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color.fromARGB(197, 94, 94, 94)),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  note.title,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  note.createdDate.toString().substring(0, 10),
+                  style: const TextStyle(
+                      color: Color.fromARGB(200, 200, 200, 200)),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                note.content.substring(0),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ));
   }
 }
